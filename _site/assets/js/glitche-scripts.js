9b5cdd67f24f73f4612569b5a2feb6f11b278856
Jordan Meyer <jordanmey@zillowgroup.com>
2021-02-20 11:41:47 -0500

    dropped page fade to test ios

--- _site/assets/js/glitche-scripts.js
+++ _site/assets/js/glitche-scripts.js
@@ -50,26 +50,26 @@ $(function () {
         });
     });
 
-    /*Fade-out animation between load pages*/
-    $('header .top-menu, .typed-bread').on('click', 'a', function(){
-        var link = $(this).attr('href');
-        if(link.indexOf('#section-') == 0){
-            if(!$('body').hasClass('home')){
-                location.href = '/'+link;
-            }
+    // /*Fade-out animation between load pages*/
+    // $('header .top-menu, .typed-bread').on('click', 'a', function(){
+    //     var link = $(this).attr('href');
+    //     if(link.indexOf('#section-') == 0){
+    //         if(!$('body').hasClass('home')){
+    //             location.href = '/'+link;
+    //         }
 
-            $('body, html').animate({scrollTop: $(link).offset().top - 110}, 400);
-            if($('header').hasClass('active')){
-                $('.menu-btn').trigger('click');
-            }
-        } else {
-            $('body').removeClass('loaded');
-            setTimeout(function() {
-                location.href = "" + link;
-            }, 500);
-        }
-        return false;
-    });
+    //         $('body, html').animate({scrollTop: $(link).offset().top - 110}, 400);
+    //         if($('header').hasClass('active')){
+    //             $('.menu-btn').trigger('click');
+    //         }
+    //     } else {
+    //         $('body').removeClass('loaded');
+    //         setTimeout(function() {
+    //             location.href = "" + link;
+    //         }, 500);
+    //     }
+    //     return false;
+    // });
 
     /*Menu mobile*/
     $('header').on('click', '.menu-btn', function(){
